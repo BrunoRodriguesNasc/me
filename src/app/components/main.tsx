c5b3a6257d6970eb367313a410ad944bd7bcb3f2
Bruno Rodrigues <72079884+BrunoRodriguesNasc@users.noreply.github.com>
2024-10-27 12:46:22 -0300

    Update main.tsx

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -8,11 +8,11 @@ const Main: React.FC = () => {
         <div className="md:w-1/2 p-4">
           <h1 className="text-4xl font-bold mb-4">Bruno Rodrigues</h1>
           <p className="text-lg ">
-            Hey there! Im Bruno Rodrigues, a passionate Full Stack Developer
+                 Hey there! I&lsquo;m Bruno Rodrigues, a passionate Full Stack Developer
             with a knack for bringing ideas to life through code. With years of
-            experience building scalable web applications, I've worked with some
-            of Brazil largest companies, creating APIs and robust solutions to
-            enhance user experiences. I’m proficient in a wide range of
+            experience building scalable web applications, I&apos;ve worked with some
+            of Brazil&apos;s largest companies, creating APIs and robust solutions to
+            enhance user experiences. II&lsquo;m proficient in a wide range of
             technologies like JavaScript, Next.js, React, PHP, and Node.js, and
             I enjoy tackling complex problems head-on. My goal is to use my
             skills to empower people and create impactful solutions that make a
